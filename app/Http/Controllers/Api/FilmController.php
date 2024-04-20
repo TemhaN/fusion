0d3118d32917febd9f4767cdd24e89e5ac38de50
TemhaN <temhan2005s@gmail.com>
2024-04-20 10:54:14 +0500

    fixes bugs

--- app/Http/Controllers/Api/FilmController.php
+++ app/Http/Controllers/Api/FilmController.php
@@ -13,7 +13,7 @@ class FilmController extends Controller
     public function index(Request $request)
     {
         $page = $request->query('page', 1);
-        $size = $request->query('size', 10);
+        $size = $request->query('size', 30);
         $sortBy = $request->query('sortBy', 'name');
         $sortDir = $request->query('sortDir', 'asc');
 
