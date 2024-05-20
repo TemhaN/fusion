585c029a645192fc6b7d8f1fc9832614d07b8d15
TemhaN <temhan2005s@gmail.com>
2024-05-20 08:12:41 +0500

    COPYRIGHT

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,3 +1,9 @@
+{{-- This is the source code.
+
+I do not discourage or prohibit the use of my code, but please be respectful and credit the author.
+
+Copyright @TemhaN, 2024 --}}
+
 <!DOCTYPE html>
 <html lang="en">
 
